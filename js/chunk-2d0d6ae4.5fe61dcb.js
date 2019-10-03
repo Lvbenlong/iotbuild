405fce3afc5f17b4d5f30b8d3a273d836abccb7a
bestkit <bestkit@bestkitdeiMac.local>
2019-10-04 00:18:45 +0800

    for code review v1.1

---
+++ js/chunk-2d0d6ae4.5fe61dcb.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0d6ae4"],{"741b":function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",{staticClass:"help"})},c=[],s={name:"Help"},l=s,u=t("2877"),i=Object(u["a"])(l,a,c,!1,null,"d4eacc56",null);n["default"]=i.exports}}]);
+//# sourceMappingURL=chunk-2d0d6ae4.5fe61dcb.js.map
